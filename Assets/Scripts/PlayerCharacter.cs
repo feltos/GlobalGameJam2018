20b37b76c3674f76d1dd51a831ad15820e2923f8
DESKTOP-89KHFO2\guill <guillaumevolgyesi@gmail.com>
2018-01-27 14:43:20 +0000

    finish the playerCharacter movement and spring anim

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour {
+public class PlayerCharacter : MonoBehaviour
+{
+    Rigidbody2D body;
+    float horizontal;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpForce;
+    [SerializeField]
+    GameObject groundCheck;
+    bool isGrounded;
+    [SerializeField]
+    float springForce;
+    [SerializeField]
+    Animator springAnim;
+    [SerializeField]
+    BoxCollider2D springCollider;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(speed * horizontal, body.velocity.y);
+        isGrounded = Physics2D.Linecast(transform.position, groundCheck.transform.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            body.velocity = body.velocity + Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Traps"))
+        {
+            //call other scripts TrapsManager
+        }
+
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Spring"))
+        {
+            springAnim.SetInteger("SpringTransition", 1);
+            body.velocity = new Vector2(body.velocity.x, body.velocity.y + springForce);
+           
+        }
+    }
 }
