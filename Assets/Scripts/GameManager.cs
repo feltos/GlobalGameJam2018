1925da98bee8efcbd01b988247f2187473702ef8
Surue <nicolas06062000@hotmail.com>
2018-01-28 15:36:48 +0100

    add time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+    [SerializeField]
+    Text text;
 
     [SerializeField]
     float timeForARound = 10;
@@ -12,7 +15,8 @@ public class GameManager : MonoBehaviour {
         P1_PLAYING,
         P1_BUILDING,
         P2_PLAYING,
-        P2_BUILDING
+        P2_BUILDING,
+        END
     }
 
     [SerializeField]
@@ -27,14 +31,23 @@ public class GameManager : MonoBehaviour {
 
     LevelController levelController;
     BuildManager buildManager;
+    TimeRemaning timeRemaning;
+
+    int round = 0;
+    const int MAX_ROUND = 8;
 
     State state = State.IDLE;
     float currentTimer = 0;
 
     bool objectHasBeenDrop = false;
 
+    int p1win = 0;
+    int p2win = 0;
+
 	// Use this for initialization
 	void Start () {
+        timeRemaning = FindObjectOfType<TimeRemaning>();
+
         state = State.P1_BUILDING;
         buildManager = FindObjectOfType<BuildManager>();
         levelController = FindObjectOfType<LevelController>();
@@ -57,15 +70,21 @@ public class GameManager : MonoBehaviour {
                 break;
 
             case State.P1_BUILDING:
-                if(!buildManager.IsRunning()) {
-                    buildManager.Launch();
-                }
-                if(objectHasBeenDrop) {
-                    currentTimer = 0;
-                    state = State.P2_PLAYING;
-                    player2Instance = Instantiate(player2Prefab, levelController.GetStartPosition(), Quaternion.identity);
-                    buildManager.Stop();
-                    objectHasBeenDrop = false;
+                if(round == MAX_ROUND) {
+                    state = State.END;
+                } else {
+                    if(!buildManager.IsRunning()) {
+                        buildManager.Launch();
+                    }
+                    if(objectHasBeenDrop) {
+                        currentTimer = 0;
+                        state = State.P2_PLAYING;
+                        player2Instance = Instantiate(player2Prefab, levelController.GetStartPosition(), Quaternion.identity);
+                        buildManager.Stop();
+                        objectHasBeenDrop = false;
+                        timeRemaning.SetTimeForLevel(timeForARound);
+                        round++;
+                    }
                 }
                 break;
 
@@ -80,15 +99,31 @@ public class GameManager : MonoBehaviour {
                 break;
 
             case State.P2_BUILDING:
-                if(!buildManager.IsRunning()) {
-                    buildManager.Launch();
+                if(round == MAX_ROUND) {
+                    state = State.END;
+                } else {
+                    if(!buildManager.IsRunning()) {
+                        buildManager.Launch();
+                    }
+                    if(objectHasBeenDrop) {
+                        currentTimer = 0;
+                        state = State.P1_PLAYING;
+                        player1Instance = Instantiate(player1Prefab, levelController.GetStartPosition(), Quaternion.identity);
+                        buildManager.Stop();
+                        objectHasBeenDrop = false;
+                        timeRemaning.SetTimeForLevel(timeForARound);
+                        round++;
+                    }
                 }
-                if(objectHasBeenDrop) {
-                    currentTimer = 0;
-                    state = State.P1_PLAYING;
-                    player1Instance = Instantiate(player1Prefab, levelController.GetStartPosition(), Quaternion.identity);
-                    buildManager.Stop();
-                    objectHasBeenDrop = false;
+                break;
+
+            case State.END:
+                if(p1win == p2win) {
+                    text.text = "DRAW";
+                }else if(p1win > p2win) {
+                    text.text = "PLAYER 1 WIN";
+                } else {
+                    text.text = "PLAYER 2 WIN";
                 }
                 break;
         }
@@ -100,6 +135,12 @@ public class GameManager : MonoBehaviour {
 
     public void PlayerWin() {
         currentTimer = timeForARound;
+        if(state == State.P1_PLAYING) {
+            p1win++;
+        }
+        if(state == State.P2_PLAYING) {
+            p2win++;
+        }
     }
 
     public void PlayerDie() {
---
+++ Assets/Scripts/TimeRemaning.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeRemaning : MonoBehaviour {
+
+    [SerializeField]
+    Text textSeconds;
+
+    float timeInSeconds = 0;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void SetTimeForLevel(float time) {
+        timeInSeconds = time;
+        StartCoroutine(ClockAnimation());
+    }
+
+    IEnumerator ClockAnimation() {
+        while(timeInSeconds > 0) {
+            timeInSeconds -= 1;
+            DisplayTime();
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void DisplayTime() {
+        if((timeInSeconds % 60) < 10) {
+            textSeconds.text = "0" + (timeInSeconds % 60).ToString();
+        } else {
+            textSeconds.text = (timeInSeconds % 60).ToString();
+        }
+    }
+}
